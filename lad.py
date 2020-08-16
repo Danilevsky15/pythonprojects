e6f02c0f2f3508c83e2f0b9914d199f89adc87a7
Daniil <golddaniiil13@gmail.com>
2020-08-16 21:43:57 +0300

    lad/

--- lad.py
+++ lad.py
@@ -3,40 +3,40 @@ if __name__ == "__main__":
     int('9900')
     int('42354')
 
-    b = 10*50
-    v = 8**8
-    c = 100/10
-    h = 100/45
-    z = 80//9
-    f = 80-50
-    g = 50+70
+    b1 = 10*50
+    v1 = 8**8
+    c1 = 100/10
+    h1 = 100/45
+    z1 = 80//9
+    f1 = 80-50
+    g1 = 50+70
 
-    print(b, v, c, h, z, f, g)
+    print(b1, v1, c1, h1, z1, f1, g1)
 
-days = { "days":"Понедельник\n" "вторник\n" "среда\n" "четверг\n" "пятница\n" "суббота \n" "воскресенье\n" }
-
-print(days)
-
-#
-
-days2 = "\nДни недели:\n" "    Понедельник\n" "    Вторник\n" "    Среда\n"\
-        "    Четверг\n" "    Пятница\n" "    Суббота\n" "    Воскресенье\n"
-
-print(days2)
-
-#
-
-a = ' \t Пн\t вт\t среда\t четверг\t пятница\t суббота '
+a = { "days":"Понедельник\n" "вторник\n" "среда\n" "четверг\n" "пятница\n" "суббота \n" "воскресенье\n" }
 
 print(a)
 
 #
 
-one = 'asdasd / dgsdgd / eqtwre / bhdsgsdh / safsfasffsaf / asdqwfewf'
-one2 = 'asdasd . gegw . assxxvr . ergregergre . geerger'
-one3 = 'dasdger - dasfdsfasdsf - sfasfsafsfasfasfasfasf'
-one4 = "dasdasdasd "
-print( one.split('/'), one2.split('.'), one3.split('-'), one4   .split('/') )
+b2 = "\nДни недели:\n" "    Понедельник\n" "    Вторник\n" "    Среда\n"\
+        "    Четверг\n" "    Пятница\n" "    Суббота\n" "    Воскресенье\n"
+
+print(b2)
+
+#
+
+c2 = ' \t Пн\t вт\t среда\t четверг\t пятница\t суббота '
+
+print(c2)
+
+#
+
+d2 = 'asdasd / dgsdgd / eqtwre / bhdsgsdh / safsfasffsaf / asdqwfewf'
+d3 = 'asdasd . gegw . assxxvr . ergregergre . geerger'
+d4 = 'dasdger - dasfdsfasdsf - sfasfsafsfasfasfasfasf'
+d5 = "dasdasdasd "
+print( d2.split('/'), d3.split('.'), d4.split('-'), d5.split('/') )
 
 
 pythons = {
@@ -56,4 +56,67 @@ print(pythons.update(others))
 
 print(pythons.clear())
 
-print(pythons['Cleese'])
+c3='012345'
+c4='678910erwerrewewr'
+c5 = c3+c4
+print(c5)
+
+v2 = 'a' , 'e' , 'b' , 'd', 'c' , 'g' ,'f' ,'l'
+print(sorted(v2))
+print(len(v2))
+
+a55=['sdfsdfds' , ' dsfsdfsdf' , 'sdfdfsdf']
+b88=a55.copy()
+print(b88)
+
+t = ['325 * 40']
+y = t.copy()
+print(y)
+
+
+l = [1,4,5,2,7,3]
+l.sort()
+print(l)
+
+
+t = [4564, 82626 ,  40 , 87 ,  782]
+t.sort()
+print (t)
+
+
+t = [8234098 , 801, 923, 9100000000000, 3, 4, 1, 2, 5, 6, 7 , 9,8]
+t.sort()
+print (t)
+
+gg = ['a' , 'b', 't', 'e', 't' , 'c', 'd', 'z']
+gg.sort()
+print(gg)
+
+
+e = 'asdasasd' , 'asdsdasaqw ' , 'rwetewtwe' , 'kboivbc' , 'kvmxlcf' , 'fkoxcov'
+print(len(e))
+
+g2 = 'dasdasd' , 'sadsad' ,'dasdasd' , 'dasdasd'
+print(g2)
+
+a1 , g3 = 'fsddf' , 'frererere'
+print (a1 , g3)
+
+
+a2 = {
+    "day": "A period of twenty-four hours, mostly misspent",
+    "positive": "Mistaken at the top of one's voice",
+    "misfortune": "The kind of fortune that never misses",
+}
+print(a2)
+
+or2 = {
+    'sdfs' : 'akflkdsf' ,
+    'kjkgskjds' : 'mvxma' ,
+    'jkgdk' : 'kvmaasd' ,
+}
+
+a6 = [ ['a', 'c'], ['z', 'k'] ]
+print(dict(a6))
+
+
