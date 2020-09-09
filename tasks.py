8f832be19bf6e3d60d039e25e8225bc8087b97b4
Daniil <golddaniiil13@gmail.com>
2020-09-09 19:05:09 +0300

    commit

---
+++ tasks.py
@@ -0,0 +1,24 @@
+if __name__ == "__main__":
+
+    # задача 108
+
+    input = 1
+    output = input
+    print(output)
+
+    input0 = 2
+    output0 = input0
+    print(input0)
+
+    # задача 108
+
+
+
+    # задача 1
+
+    input1 = 1 + 4
+    output1 = input1
+    print(output1)
+
+    # задача 1
+
