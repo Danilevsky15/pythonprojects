f70882bbd3c91d749100e9674bb3b495efec6fa0
Daniil <golddaniiil13@gmail.com>
2020-10-30 07:52:53 +0300

    yjdasdasdasdk a;sa

---
+++ олимпиада.py
@@ -0,0 +1,24 @@
+#бисер https://acmp.ru/index.asp?main=task&id_task=903
+a = int(input("рандомное число:"))
+print(a+1)
+
+
+# https://acmp.ru/index.asp?main=task&id_task=942
+a = (1)
+b = (2)
+c = (3)
+print("Победил" + a + "-ый школьник")
+
+
+# https://acmp.ru/index.asp?main=task&id_task=25
+a = int(input("Введите первое число:"))
+b = int(input("Введите второе число:"))
+if a > b :
+    print(">")
+if a < b :
+    print("<")
+
+
+# https://acmp.ru/index.asp?main=task&id_task=766
+n,m,k = int
+n,m = int(float(input()))
